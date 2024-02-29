9101810c7b30857047c9e1295cfe0b1e2fff1e8d
NabiryeJoan <nabiryejoan381@gmail.com>
2024-02-29 17:29:56 +0300

    Refactor: Tourist session

--- Controllers/tourist_controllers.py
+++ Controllers/tourist_controllers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException
 from Models.models import Tourist
 from Connections.connections import session
 from hashing import Harsher
+from sqlalchemy.orm import Session
 import secrets
 import smtplib
 from datetime import datetime
@@ -19,18 +20,18 @@ from Connections.token_and_keys import (
     TWILIO_PHONE_NUMBER
 )
 
-def create_tourist(new_tourist: dict):
+def create_tourist(db:Session,new_tourist: dict):
     tourist = Tourist.create_tourist_data(new_tourist['name'], new_tourist['email'], new_tourist['phonenumber'], new_tourist['number'], new_tourist['status'])
     try:   
-        session.add(tourist)
-        session.commit()
-        session.refresh(tourist)
+        db.add(tourist)
+        db.commit()
+        db.refresh(tourist)
         create_welcome_email(new_tourist)
         return {"message":"Tourist created successfully","status":200}
     
     except Exception as e:
         print(f"Error occured: {e}")
-        session.rollback()
+        db.rollback()
         return {"message":"An error occured","status":500}
     
 def create_welcome_email(tourist_data):
@@ -60,27 +61,27 @@ def create_welcome_email(tourist_data):
     server.quit()
    
 
-def get_all_tourists():
-    tourists = Tourist.get_all_tourist_data(session)
+def get_all_tourists(db:Session):
+    tourists = Tourist.get_all_tourist_data(db)
     return tourists
 
-def get_tourist_by_id(id):
-    tourist = Tourist.get_tourist_data(session, id)
+def get_tourist_by_id(db:Session,id):
+    tourist = Tourist.get_tourist_data(db, id)
     if tourist:
         return tourist
     else:
         raise HTTPException(status_code=404, detail="Tourist not found")
     
-def update_tourist(data):
-    tourist = Tourist.update_tourist_data(session, data)
+def update_tourist(db:Session,data):
+    tourist = Tourist.update_tourist_data(db, data)
     if tourist:
         return tourist
     else:
         raise HTTPException(status_code=404, detail="Tourist not found")
     
-def delete_tourist(id_data):
+def delete_tourist(db:Session,id_data):
     id = id_data['tourist_id']
-    tourist = Tourist.delete_tourist_data(session, id)
+    tourist = Tourist.delete_tourist_data(db, id)
     if tourist:
         return tourist
     else:
--- Routes/Tourist_routes.py
+++ Routes/Tourist_routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+from Connections.connections import SessionLocal
 from Controllers.tourist_controllers import (
     create_tourist,
     get_all_tourists,
@@ -8,47 +10,53 @@ from Controllers.tourist_controllers import (
 )
 
 router = APIRouter()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @router.get("/")
 async def read_root():
     return {"Tourists" : "Hello World"}
 
 @router.post("/create_tourist")
-async def create_tourist_route(new_tourist: dict):
+async def create_tourist_route(new_tourist: dict,db: Session = Depends(get_db)):
     try:
-        tourist = create_tourist(new_tourist)
+        tourist = create_tourist(db,new_tourist)
         return tourist
     except Exception as e:
         return HTTPException(status_code=400, detail=str(e))
     
 @router.get("/get_all_tourists")
-async def get_all_tourists_route():
+async def get_all_tourists_route(db: Session = Depends(get_db)):
     try:
-        tourists = get_all_tourists()
+        tourists = get_all_tourists(db)
         return tourists
     except Exception as e:
         return HTTPException(status_code=400, detail=str(e))
     
 @router.delete("/delete_tourist/")
-async def delete_tourist_route(id: dict):
+async def delete_tourist_route(id: dict,db: Session = Depends(get_db)):
     try:
-        tourist = delete_tourist(id)
+        tourist = delete_tourist(db,id)
         return tourist
     except Exception as e:
         return HTTPException(status_code=400, detail=str(e))
     
 @router.put("/update_tourist")
-async def update_tourist_route(tourist: dict):
+async def update_tourist_route(tourist: dict,db: Session = Depends(get_db)):
     try:
-        tourist = update_tourist(tourist)
+        tourist = update_tourist(db,tourist)
         return tourist
     except Exception as e:
         return HTTPException(status_code=400, detail=str(e))
     
 @router.get("/get_tourist_by_id/")
-async def get_tourist_by_id_route(tourist_id: int):
+async def get_tourist_by_id_route(tourist_id: int, db: Session = Depends(get_db)):	
     try:
-        tourist = get_tourist_by_id(tourist_id)
+        tourist = get_tourist_by_id(db,tourist_id)
         return tourist
     except Exception as e:
         return HTTPException(status_code=400, detail=str(e))
